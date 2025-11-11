cea8c6cfd4bccfc3fe1fb511668a391974b21e8d
thaprohit <thaparohit155@gmail.com>
2025-11-11 08:48:42 +0545

    Updated

--- lib/copywith.dart
+++ lib/copywith.dart
@@ -1,4 +1,3 @@
-
 class User {
   final String name;
   final int age;
--- lib/future.dart
+++ lib/future.dart
@@ -1,23 +1,15 @@
-class User {
-  final String name;
-  final int age;
-
-  const User(this.name, this.age);
-
-  User copyWith({String? name, int? age}) {
-    return User(
-      name ?? this.name, 
-      age ?? this.age,   
-    );
-  }
+Future<String> fetchUserName() {
+  return Future.delayed(Duration(seconds: 2), () {
+    return "Rohit";
+  });
 }
 
 void main() {
-  final user1 = User("Rohit", 19);
+  print("Fetching username...");
 
- 
-  final user2 = user1.copyWith(age: 20);
+  fetchUserName().then((name) {
+    print("Username: $name");
+  });
 
-  print("Original: ${user1.name}, ${user1.age}"); 
-  print("Updated: ${user2.name}, ${user2.age}");  
+  print("Done calling fetchUserName()");
 }
